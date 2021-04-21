f1684be344a7b074b076722add329aefbaa819d9
Paul Frolke <p.r.frolke@student.tudelft.nl>
2021-04-21 17:15:51 +0200

    change timeout to interval

--- script.js
+++ script.js
@@ -10,104 +10,106 @@ function getParameterByName(variable) {
 }
 
 window.addEventListener("load", function () {
-  setTimeout(function () {
-    /* weather update */
-    var appid = getParameterByName("appid");
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", function () {
-      var weatherData = JSON.parse(this.responseText);
-      var weatherTemp = document.getElementById("weather-temp");
-      var temp = Math.round(weatherData.main.temp);
-      weatherTemp.innerHTML = temp;
-    });
-    oReq.open(
-      "GET",
-      "https://api.openweathermap.org/data/2.5/weather?id=2757345&units=metric&appid=" +
-        appid
-    );
-    oReq.send();
-
-    /* greeting update */
-    var greeting = document.getElementById("greeting");
-    var date = new Date();
-
-    var greet = "Mogguh";
-    if (date.getHours() > 12) {
-      greet = "Hallo";
-    }
-    if (date.getHours() > 22 || date.getHours < 5) {
-      greet = "Truste";
-    }
-
-    greeting.innerHTML = greet + " Paul";
-
-    /* datetime update */
-    var timedate = document.getElementById("timedate");
-
-    var minTextIndex = Math.round((date.getMinutes() / 60) * 12) % 12;
-    var minText = [
-      "",
-      "Vijf over",
-      "Tien over",
-      "Kwart over",
-      "Tien voor half",
-      "Vijf voor half",
-      "Half",
-      "Vijf over half",
-      "Tien over half",
-      "Kwart voor",
-      "Tien voor",
-      "Vijf voor",
-    ];
-
-    var hourTextIndex = date.getHours();
-    if (minTextIndex >= 4) {
-      hourTextIndex++;
-    }
-    hourTextIndex = hourTextIndex % 12;
-    var hourText = [
-      "twaalf",
-      "één",
-      "twee",
-      "drie",
-      "vier",
-      "vijf",
-      "zes",
-      "zeven",
-      "acht",
-      "negen",
-      "tien",
-      "elf",
-    ];
-
-    var timeText;
-
-    if (minTextIndex == 0) {
-      timeText = timeText.charAt(0).toUpperCase() + timeText.slice(1) + " uur";
-    } else {
-      timeText = minText[minTextIndex] + " " + hourText[hourTextIndex];
-    }
-
-    var monthIndex = date.getMonth();
-    var monthText = [
-      "Jan",
-      "Feb",
-      "Mrt",
-      "Apr",
-      "Mei",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Okt",
-      "Nov",
-      "Dec",
-    ][monthIndex];
-
-    var dayText = date.getDate();
-
-    var datetimeText = timeText + ", " + dayText + " " + monthText;
-
-    timedate.innerHTML = datetimeText;
-  }, 120000);
+  setInterval(update, 120000);
 });
+
+function update() {
+	/* weather update */
+	var appid = getParameterByName("appid");
+	var oReq = new XMLHttpRequest();
+	oReq.addEventListener("load", function () {
+		var weatherData = JSON.parse(this.responseText);
+		var weatherTemp = document.getElementById("weather-temp");
+		var temp = Math.round(weatherData.main.temp);
+		weatherTemp.innerHTML = temp;
+	});
+	oReq.open(
+		"GET",
+		"https://api.openweathermap.org/data/2.5/weather?id=2757345&units=metric&appid=" +
+			appid
+	);
+	oReq.send();
+
+	/* greeting update */
+	var greeting = document.getElementById("greeting");
+	var date = new Date();
+
+	var greet = "Mogguh";
+	if (date.getHours() > 12) {
+		greet = "Hallo";
+	}
+	if (date.getHours() > 22 || date.getHours < 5) {
+		greet = "Truste";
+	}
+
+	greeting.innerHTML = greet + " Paul";
+
+	/* datetime update */
+	var timedate = document.getElementById("timedate");
+
+	var minTextIndex = Math.round((date.getMinutes() / 60) * 12) % 12;
+	var minText = [
+		"",
+		"Vijf over",
+		"Tien over",
+		"Kwart over",
+		"Tien voor half",
+		"Vijf voor half",
+		"Half",
+		"Vijf over half",
+		"Tien over half",
+		"Kwart voor",
+		"Tien voor",
+		"Vijf voor",
+	];
+
+	var hourTextIndex = date.getHours();
+	if (minTextIndex >= 4) {
+		hourTextIndex++;
+	}
+	hourTextIndex = hourTextIndex % 12;
+	var hourText = [
+		"twaalf",
+		"één",
+		"twee",
+		"drie",
+		"vier",
+		"vijf",
+		"zes",
+		"zeven",
+		"acht",
+		"negen",
+		"tien",
+		"elf",
+	];
+
+	var timeText;
+
+	if (minTextIndex == 0) {
+		timeText = timeText.charAt(0).toUpperCase() + timeText.slice(1) + " uur";
+	} else {
+		timeText = minText[minTextIndex] + " " + hourText[hourTextIndex];
+	}
+
+	var monthIndex = date.getMonth();
+	var monthText = [
+		"Jan",
+		"Feb",
+		"Mrt",
+		"Apr",
+		"Mei",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Okt",
+		"Nov",
+		"Dec",
+	][monthIndex];
+
+	var dayText = date.getDate();
+
+	var datetimeText = timeText + ", " + dayText + " " + monthText;
+
+	timedate.innerHTML = datetimeText;
+}
