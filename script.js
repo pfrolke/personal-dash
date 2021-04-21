fb6e8edf1ed3cec0d71bdd3cbf0dc524315d37f0
Paul Frolke <p.r.frolke@student.tudelft.nl>
2021-04-21 18:08:23 +0200

    trello todo list updater

--- script.js
+++ script.js
@@ -10,114 +10,139 @@ function getParameterByName(variable) {
 }
 
 window.addEventListener("load", function () {
-	update();
+  update();
   setInterval(update, 120000);
 });
 
 function update() {
-	updateWeather();
-	updateGreeting();
-	updateTimeDate();
+  updateWeather();
+  updateGreeting();
+  updateTimeDate();
+  updateTodos();
 }
 
 function updateWeather() {
-	var appid = getParameterByName("appid");
-	var oReq = new XMLHttpRequest();
-	oReq.addEventListener("load", function () {
-		var weatherData = JSON.parse(this.responseText);
-		var weatherTemp = document.getElementById("weather-temp");
-		var temp = Math.round(weatherData.main.temp);
-		weatherTemp.innerHTML = temp;
-	});
-	oReq.open(
-		"GET",
-		"https://api.openweathermap.org/data/2.5/weather?id=2757345&units=metric&appid=" +
-			appid
-	);
-	oReq.send();
+  var appid = getParameterByName("appid");
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", function () {
+    var weatherData = JSON.parse(this.responseText);
+    var weatherTemp = document.getElementById("weather-temp");
+    var temp = Math.round(weatherData.main.temp);
+    weatherTemp.innerHTML = temp;
+  });
+  oReq.open(
+    "GET",
+    "https://api.openweathermap.org/data/2.5/weather?id=2757345&units=metric&appid=" +
+      appid
+  );
+  oReq.send();
 }
 
 function updateGreeting() {
-	var greeting = document.getElementById("greeting");
-	var date = new Date();
+  var greeting = document.getElementById("greeting");
+  var date = new Date();
 
-	var greet = "Mogguh";
-	if (date.getHours() > 12) {
-		greet = "Hallo";
-	}
-	if (date.getHours() > 22 || date.getHours < 5) {
-		greet = "Truste";
-	}
+  var greet = "Mogguh";
+  if (date.getHours() > 12) {
+    greet = "Hallo";
+  }
+  if (date.getHours() > 22 || date.getHours < 5) {
+    greet = "Truste";
+  }
 
-	greeting.innerHTML = greet + " Paul";
+  greeting.innerHTML = greet + " Paul";
 }
 
 function updateTimeDate() {
-	var timedate = document.getElementById("timedate");
+  var timedate = document.getElementById("timedate");
 
-	var minTextIndex = Math.round((date.getMinutes() / 60) * 12) % 12;
-	var minText = [
-		"",
-		"Vijf over",
-		"Tien over",
-		"Kwart over",
-		"Tien voor half",
-		"Vijf voor half",
-		"Half",
-		"Vijf over half",
-		"Tien over half",
-		"Kwart voor",
-		"Tien voor",
-		"Vijf voor",
-	];
+  var minTextIndex = Math.round((date.getMinutes() / 60) * 12) % 12;
+  var minText = [
+    "",
+    "Vijf over",
+    "Tien over",
+    "Kwart over",
+    "Tien voor half",
+    "Vijf voor half",
+    "Half",
+    "Vijf over half",
+    "Tien over half",
+    "Kwart voor",
+    "Tien voor",
+    "Vijf voor",
+  ];
 
-	var hourTextIndex = date.getHours();
-	if (minTextIndex >= 4) {
-		hourTextIndex++;
-	}
-	hourTextIndex = hourTextIndex % 12;
-	var hourText = [
-		"twaalf",
-		"één",
-		"twee",
-		"drie",
-		"vier",
-		"vijf",
-		"zes",
-		"zeven",
-		"acht",
-		"negen",
-		"tien",
-		"elf",
-	];
+  var hourTextIndex = date.getHours();
+  if (minTextIndex >= 4) {
+    hourTextIndex++;
+  }
+  hourTextIndex = hourTextIndex % 12;
+  var hourText = [
+    "twaalf",
+    "één",
+    "twee",
+    "drie",
+    "vier",
+    "vijf",
+    "zes",
+    "zeven",
+    "acht",
+    "negen",
+    "tien",
+    "elf",
+  ];
 
-	var timeText;
+  var timeText;
 
-	if (minTextIndex == 0) {
-		timeText = timeText.charAt(0).toUpperCase() + timeText.slice(1) + " uur";
-	} else {
-		timeText = minText[minTextIndex] + " " + hourText[hourTextIndex];
-	}
+  if (minTextIndex == 0) {
+    timeText = timeText.charAt(0).toUpperCase() + timeText.slice(1) + " uur";
+  } else {
+    timeText = minText[minTextIndex] + " " + hourText[hourTextIndex];
+  }
 
-	var monthIndex = date.getMonth();
-	var monthText = [
-		"Jan",
-		"Feb",
-		"Mrt",
-		"Apr",
-		"Mei",
-		"Jun",
-		"Jul",
-		"Aug",
-		"Sep",
-		"Okt",
-		"Nov",
-		"Dec",
-	][monthIndex];
+  var monthIndex = date.getMonth();
+  var monthText = [
+    "Jan",
+    "Feb",
+    "Mrt",
+    "Apr",
+    "Mei",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Okt",
+    "Nov",
+    "Dec",
+  ][monthIndex];
 
-	var dayText = date.getDate();
+  var dayText = date.getDate();
 
-	var datetimeText = timeText + ", " + dayText + " " + monthText;
+  var datetimeText = timeText + ", " + dayText + " " + monthText;
 
-	timedate.innerHTML = datetimeText;
+  timedate.innerHTML = datetimeText;
+}
+
+function updateTodos() {
+  var trellokey = getParameterByName("trellokey");
+  var trellotoken = getParameterByName("trellotoken");
+
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", function () {
+    var res = JSON.parse(this.responseText);
+    var todosList = document.getElementById("todos-list");
+
+    for (var i = 0; i < res.length; i++) {
+      todosList.innerHTML += "<li>" + res[i].name + "</li>";
+    }
+  });
+
+  oReq.open(
+    "GET",
+    "https://api.trello.com/1/lists/5dc1b5133037ac752d6b4834/cards?key=" +
+      trellokey +
+      "&token=" +
+      trellotoken
+  );
+  oReq.send();
 }
